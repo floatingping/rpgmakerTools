ceeeadb922017b5cfd808348fd762ed9dada2971
floatingping <floatingping@gmail.com>
2022-08-21 10:09:39 +0800

    feat: add Lines Formater

--- tools/jsTools.js
+++ tools/jsTools.js
@@ -9,7 +9,23 @@ const vueJsTools = new Vue({
         regexp: `/\\s*:.*,/`,
         keyWordSelecterOut: "",
         classKeys: "name\nage\nmoney",
-        jsClass: ""
+        jsClass: "",
+        formatSrcData: `Alice
+Bob 
+Carol`,
+        formatCallbacks: [
+            "str=> `${str.trim()}`",
+            "str=> `${str.trim()},`",
+            "str=> `'${str.trim()}',`",
+            "str=> `${str.replaceAll(/s*:.*,/g, '')}`",
+            `s=>{
+    return s
+        .replaceAll(/s*:.*,/g, "")
+        .trim();
+}`
+        ],
+        formatDistData: "",
+        formatComboIndexs: "3,0"
     },
     methods: {
         seelctKeywords: function () {
@@ -17,7 +33,7 @@ const vueJsTools = new Vue({
             if (!/^\/.*\/$/.test(this.regexp)) return alert("bad regexp");
             this.keyWordSelecterOut = this.keyWordSelecterIn
                 .replaceAll(new RegExp(this.regexp.substring(1, this.regexp.length - 1), "g"), "")
-                .replaceAll(/\n\s+/g,"\n");
+                .replaceAll(/\n\s+/g, "\n");
         },
         generate: function () {
             const keys = this.classKeys.replaceAll("\r\n", "\n")
@@ -35,6 +51,35 @@ ${constructorData}
 ${geters}
 ${setters}
 }`;
-        }
+        },
+        convertDataWithFormatRules: function (arrayString, formatStrings) {
+            const formatRule = s => formatStrings.reduce((a, c) => eval(c)(a), s);
+            return arrayString.map(formatRule);
+        },
+        executeFormat: function (idxsFormatCallbacks) {
+            try {
+                this.formatDistData = this.convertDataWithFormatRules(
+                    this.formatSrcData.split("\n"),
+                    idxsFormatCallbacks.map(i => this.formatCallbacks[i])
+                ).join("\n");
+            }
+            catch(e) {
+                console.error(e);
+                alert("fortmat fail!");
+            }
+        },
+        addFormatRule: function (idxFormatCallbacks) {
+            this.formatCallbacks.splice(idxFormatCallbacks + 1, 0, "");
+        },
+        executeComboFormater: function () {
+            try {
+                const ruleIdxs = this.formatComboIndexs.split(",").map(s => Number(s.trim()));
+                this.executeFormat(ruleIdxs);
+            }
+            catch (e) {
+                console.error(e);
+                alert("comboFormater fail!");
+            }
+        },
     }
 });
