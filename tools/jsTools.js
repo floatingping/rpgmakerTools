861dd007432501e6a36b388669822a1764af7505
floatingping <floatingping@gmail.com>
2022-08-21 08:52:26 +0800

    fix: JS Class Generater's format

--- tools/jsTools.js
+++ tools/jsTools.js
@@ -23,14 +23,15 @@ const vueJsTools = new Vue({
             const keys = this.classKeys.replaceAll("\r\n", "\n")
                 .split("\n")
                 .filter(s => (s?.trim() ?? "") !== "");
-            const constructorData = keys.reduce((a, c) => `${a}this._${c}=${c};\n`, "");
-            const geters = keys.reduce((a, c) => `${a}get ${c}(){ return this._${c}; }\n`, "");
-            const setters = keys.reduce((a, c) => `${a}set ${c}(val){ this._${c}=val; }\n`, "");
+            const constructorData = keys.reduce((a, c) => `${a}        this._${c}=${c};\n`, "");
+            const geters = keys.reduce((a, c) => `${a}    get ${c}(){ return this._${c}; }\n`, "");
+            const setters = keys.reduce((a, c) => `${a}    set ${c}(val){ this._${c}=val; }\n`, "");
 
             this.jsClass = `class JSClass{
     constructor({${keys.join(",")}}) {
 ${constructorData}
     }
+
 ${geters}
 ${setters}
 }`;
